c44d0c864f632c994e291516dcfe87c3a5a573a0
Hemant-Pawar-SDE <hemantpawar8230@gmail.com>
2025-06-17 23:12:22 +0530

    Http Module

---
+++ HttpModule6.js
@@ -0,0 +1,30 @@
+//how do i create a Server
+// const http=require('http')
+// const port=9090;
+
+// const server=http.createServer((req,res)=>{
+//     console.log("server created.....");
+// })
+
+// server.listen(port,()=>{
+//     console.log(`server is running at port ${port}`);
+// })
+
+
+// =============================================================================
+// const http = require('http');
+
+// const port = 4000;
+
+// // Create the server and store it in a variable
+// const server = http.createServer((req, res) => {
+
+
+//   res.writeHead(200, { "Content-Type": "text/plain" });
+//   res.end("Hello from Node.js.....");
+// });
+
+// // ✅ Call .listen on the server object, not the http module
+// server.listen(port, () => {
+//   console.log(`Server started at port ${port}`);
+// });
